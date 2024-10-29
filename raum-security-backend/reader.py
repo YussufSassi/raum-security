5c6f2022a39b1f782b98c254ba9b5839a5edee9f
Yussuf Sassi <yussuf.sassi@krankikom.biz>
2024-10-29 10:45:38 +0100

    refacter read_card function

--- raum-security-backend/reader.py
+++ raum-security-backend/reader.py
@@ -2,14 +2,15 @@ from time import sleep
 import sys
 from mfrc522 import SimpleMFRC522
 
+r = SimpleMFRC522()
 
-def read_card():
-    reader = SimpleMFRC522()
+
+def read_card(reader: SimpleMFRC522):
     id, text = reader.read()
     return id, text
 
 
 if __name__ == "__main__":
     while True:
-        print(read_card())
+        print(read_card(r))
         sleep(2)
